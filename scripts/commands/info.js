94b9ad222f1fbfaa3a64d0901949a4e298da8240
raihanbot3 <raihanislam123454321@gmail.com>
2024-07-07 15:38:13 +0600

    Update info.js

--- scripts/commands/info.js
+++ scripts/commands/info.js
@@ -41,7 +41,7 @@ var callback = () => api.sendMessage({body:`
 𝐖𝐡𝐚𝐭𝐬𝐀𝐩𝐩: wa.me/+8801762014230
 𝐓𝐞𝐥𝐞𝐠𝐫𝐚𝐦  : 𝐮𝐬𝐞 𝐤𝐨𝐫𝐢 𝐧𝐚𝐡
 𝐅𝐚𝐜𝐞𝐛𝐨𝐨𝐤 𝐋𝐢𝐧𝐤 :https://www.facebook.com/profile.php?id=61552312071816 `,attachment: fs.createReadStream(__dirname + "/cache/1.png")}, event.threadID, () => 
-    fs.unlinkSync(__dirname + "/cache/1.png"));  
+    fs.unlinkSync(__dirname + "/cache/r.png"));  
       return request(encodeURI(`https://graph.facebook.com/100000959749712/picture?height=720&width=720&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`)).pipe(
 fs.createWriteStream(__dirname+'/cache/.png')).on('close',() => callback());
    };
